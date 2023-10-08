d498aa6f90b3957929725e21434dcc8ce1562cf7
Carlos Padilla <padillabusines@gmail.com>
2023-10-08 16:24:59 -0500

    finish the work

---
+++ src/components/Participant.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { type IWeapon } from '@/views/HomeView.vue';
+
+    defineProps<{
+        title:string,
+        description:string,
+        weapons?: IWeapon[],
+        currentSelection?: IWeapon,
+        disable?: boolean,
+        winner?: boolean,
+        looser?: boolean,
+        draw?: boolean
+    }>()
+
+</script>
+
+<template>
+   <div class="participant">
+        <div class="participant-description">
+          <h2>{{ title }}</h2>
+          <p>{{ description }}</p>
+        </div>
+        <div class="participant__weapons-container">
+          <button 
+                class="participant__weapon" 
+                :disabled="disable"
+                :class="`
+                    ${currentSelection?.value === item.value?'participant__weapon--selected':''} 
+                    ${disable?'disabled':''} 
+                    ${currentSelection?.value === item.value && winner?'participant__weapon--winner':''} 
+                    ${currentSelection?.value === item.value && looser?'participant__weapon--looser':''}
+                    ${currentSelection?.value === item.value && draw?'participant__weapon--draw':''}
+                    `"
+                v-for="item in weapons"
+                @click="$emit('onSelect', item)"
+            ><p v-html="item.icon"></p></button>
+        </div>
+      </div>
+</template>
+
+<style scoped>
+.participant{
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+}
+.participant-description{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100px;
+}
+.participant__weapons-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 16px 0;
+    gap: 16px;
+}
+.participant__weapon--selected{
+    border: 3px blueviolet solid;
+}
+.participant__weapon p {
+    font-size: 25px;
+}
+.participant__weapon--winner {
+    background-color: green;
+}
+.participant__weapon--looser {
+    background-color: red;
+}
+.participant__weapon--draw {
+    background-color: orange;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,167 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { reactive } from 'vue';
+import Participant from '../components/Participant.vue'
+
+enum WeaponsType {
+  ROCK = "ROCK",
+  PAPER = "PAPER",
+  SCISSOR = "SCISSOR"
+}
+
+enum Winner {
+  PLAYER = "PLAYER",
+  MACHINE = "MACHINE",
+  DRAW = "DRAW"
+}
+
+enum MatchState {
+  WINNER = "WINNER",
+  LOOSER = 'LOOSER',
+  DRAW = "DRAW"
+}
+
+const weapons:IWeapon[] = [
+  {
+    icon: "&#128511;",
+    value: WeaponsType.ROCK,
+    weakness: [WeaponsType.PAPER],
+    strengths: [WeaponsType.SCISSOR]
+  },
+  {
+    icon: "&#129531;",
+    value: WeaponsType.PAPER,
+    weakness: [WeaponsType.SCISSOR],
+    strengths: [WeaponsType.ROCK]
+  },
+  {
+    icon: "&#9986;&#65039;",
+    value: WeaponsType.SCISSOR,
+    weakness: [WeaponsType.ROCK],
+    strengths: [WeaponsType.PAPER]
+  },
+]
+
+const playersState = reactive<{
+  isLoading: boolean;
+  playerSelection: IWeapon | null,
+  machineSelection: IWeapon | null,
+  winner: Winner | null
+}>({
+  isLoading: false,
+  playerSelection: null,
+  machineSelection: null,
+  winner: null
+})
+
+const handleWeaponPick = (weapon:IWeapon):void => {
+  const playerSelection = weapon
+
+  playersState.isLoading = true
+  playersState.winner = null
+  playersState.machineSelection = null
+  playersState.playerSelection = playerSelection
+  
+  setTimeout(()=> {
+    const machinePick = generateRandomWeaponPick()
+    playersState.machineSelection = machinePick
+    playersState.winner = calculateWinner(playerSelection as IWeapon, machinePick)
+    playersState.isLoading = false
+  }, 1000)
+
+}
+
+const calculateWinner = (player:IWeapon, machine:IWeapon):Winner  => {
+  if (machine.strengths.includes(player.value)) return Winner.MACHINE
+  else if (machine.weakness.includes(player.value)) return Winner.PLAYER
+  return Winner.DRAW
+}
+
+const generateRandomWeaponPick = ():IWeapon => {
+  const choose = Math.floor(Math.random() * weapons.length - 0.1)
+  return weapons[choose]
+}
+
+export interface IWeapon {
+  icon: string;
+  value: WeaponsType;
+  weakness: WeaponsType[];
+  strengths: WeaponsType[];
+}
+
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+
+    <section class="header">
+      <h1 class="header__title">Lest play Rock Paper Scissor !!!</h1>
+    </section>
+    <section class="content">
+      <Participant :title="'Player'" 
+      :disable="playersState.isLoading" 
+      :description="playersState.isLoading?'':'Choose a weapon!'" 
+      :weapons="weapons" 
+      @onSelect="handleWeaponPick" 
+      :winner="playersState.winner===Winner.PLAYER"
+      :looser="playersState.winner===Winner.MACHINE"
+      :draw="playersState.winner===Winner.DRAW"
+      :currentSelection="(playersState.playerSelection as IWeapon)"/>
+
+      <Participant 
+      :title="'Machine'" 
+      :disable="playersState.isLoading" 
+      :description="playersState.isLoading?'Thinking ...':''" 
+      :weapons="weapons"
+      :winner="playersState.winner===Winner.MACHINE"
+      :looser="playersState.winner===Winner.PLAYER"
+      :draw="playersState.winner===Winner.DRAW" 
+      :currentSelection="(playersState.machineSelection as IWeapon)"/>
+    
+    </section>
   </main>
 </template>
+
+<style>
+  /* GLOBAL */
+  * {
+    color: #fff;
+  }
+  .disabled {
+      opacity: .5;
+  }
+  button {
+    background-color: cornflowerblue;
+    border-radius: 5px;
+    padding: 16px;
+    border: none;
+
+  }
+  button:hover {
+    opacity: 0.7;
+  }
+  button:active {
+    opacity: 0.5;
+    
+  }
+  /* TITLE */
+  .header__title {}
+
+  /* CONTENT */
+  .content {
+    padding: 16px 0;
+    display: flex;
+    justify-content: space-between;
+  }
+  
+  /* PLAY BUTTON */
+  .footer__play-button {
+    width: 300px;
+    height: 50px;
+  }
+  .footer__play-button__text {
+    font-size: 16px;
+    font-weight: bold;
+    color: #fff;
+  }
+</style>
